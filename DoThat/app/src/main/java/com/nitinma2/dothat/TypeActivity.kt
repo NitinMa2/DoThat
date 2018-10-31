1bf894f5f1eadc6ac029cd2624014964ffafc787
NitinMa2 <nitinmathew00@gmail.com>
2018-10-31 18:43:50 +0530

    TypeActivity has content

--- DoThat/app/src/main/java/com/nitinma2/dothat/TypeActivity.kt
+++ DoThat/app/src/main/java/com/nitinma2/dothat/TypeActivity.kt
@@ -3,10 +3,13 @@ package com.nitinma2.dothat
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class TypeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_type)
+
     }
 }
